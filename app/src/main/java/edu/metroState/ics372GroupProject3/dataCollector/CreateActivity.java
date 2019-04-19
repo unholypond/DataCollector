53f19a2831ff95a6043ec7b8b2c3aa7e1e984efd
Kekeli D Akouete <dosakouete@gmail.com>
2019-04-19 16:13:51 -0500

    Dosxx update CreateActivity class, DisplayActivity class, MainActivity class, and the layouts for these activities

--- app/src/main/java/edu/metroState/ics372GroupProject3/dataCollector/CreateActivity.java
+++ app/src/main/java/edu/metroState/ics372GroupProject3/dataCollector/CreateActivity.java
@@ -1,7 +1,6 @@
 package edu.metroState.ics372GroupProject3.dataCollector;
 
 import android.app.AlertDialog;
-import android.app.Application;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -11,8 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class CreateActivity extends AppCompatActivity {
     final String TAG = "create Activity Log";
@@ -23,8 +24,9 @@ public class CreateActivity extends AppCompatActivity {
     EditText readingUnit;
     EditText readingID;
     EditText readingValue;
-    EditText readingDate;
     ProgramState createData;
+    AlertDialog.Builder builder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +42,15 @@ public class CreateActivity extends AppCompatActivity {
         readingUnit = findViewById(R.id.readingUnitField);
         readingID = findViewById(R.id.readingIDField);
         readingValue = findViewById(R.id.readingValueField);
-        readingDate = findViewById(R.id.readingDateField);
         cancelButton = findViewById(R.id.cancel);
         submitButton = findViewById(R.id.submitButton);
+        builder = new AlertDialog.Builder(this,R.style.Dialog_NoActionBar_);
+        Study currentStudy;
 
         /*
         Display the current study ID and Name to the user
          */
-        final Study currentStudy = createData.getCurrentProgramRecord().get(0);
+        currentStudy = createData.getCurrentProgramRecord().get(0);
         createData.setCurrentProgramStudy(currentStudy);
         Log.i(TAG, currentStudy.toString());
         studyId.setText(currentStudy.getStudyID());
@@ -60,41 +63,57 @@ public class CreateActivity extends AppCompatActivity {
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cancelIntent =  new Intent(CreateActivity.this, MainActivity.class);
+                Intent cancelIntent =  new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(cancelIntent);
             }
         });
 
-        /**
+        /*
          * submit the reading data to the activity that
          * will handle it
          */
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent submitReadingIntent = new Intent(getApplicationContext(), DisplayActivity.class);
-                submitReadingIntent.putExtra("the created item", "new reading");
-                /**
+
+                /*
                  * call the item factory to create item
                  */
-                //Site cSite = new Site(siteID.getText().toString());
                 createData.setCurrentProgramSite(siteID.getText().toString());
                 String id = siteID.getText().toString();
                 String type =  readingType.getText().toString();
                 String unit = readingUnit.getText().toString();
                 String rId = readingID.getText().toString();
                 double value = Double.parseDouble(readingValue.getText().toString());
-                Long date = Long.parseLong(readingDate.getText().toString());
+                Date date = new Date();
+                Long myDate = date.getTime();
                 try {
                     createData.createSite(id);
-                    createData.createReading(id, type, unit,rId, value, date);
-                    Log.i(TAG, createData.getCurrentProgramSite().toString());
+                    createData.createReading(id, type, unit,rId, value, myDate);
                     Log.i(TAG, createData.getCurrentProgramStudy().toString());
-//                    AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-//                            builder.setTitle("Confirmation")
-//                            .setMessage("Reading Created! Do you want to create another one?")
-//                            .setCancelable(true)
-//                            .setNegativeButton("Cancel", public );
+                    builder.setTitle(R.string.createDialog_title)
+                            .setMessage(R.string.createDialog_message)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    siteID.setText("");
+                                    readingType.setText("");
+                                    readingUnit.setText("");
+                                    readingID.setText("");
+                                    readingValue.setText("");
+                                    siteID.requestFocus();
+                                    Toast.makeText(getApplicationContext(),"Create new reading selected",
+                                    Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //  Action for 'NO' Button
+                                    Intent displayIntent =  new Intent(getApplicationContext(),
+                                            DisplayActivity.class);
+                                    startActivity(displayIntent);
+                                    dialog.cancel();
+                                }
+                            }).create().show();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
--- app/src/main/java/edu/metroState/ics372GroupProject3/dataCollector/DisplayActivity.java
+++ app/src/main/java/edu/metroState/ics372GroupProject3/dataCollector/DisplayActivity.java
@@ -2,12 +2,24 @@ package edu.metroState.ics372GroupProject3.dataCollector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DisplayActivity extends AppCompatActivity {
-
+    Button exportButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        exportButton = findViewById(R.id.exportButton);
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*open a dialog view to get output file name from user*/
+
+            }
+        });
+
     }
 }
